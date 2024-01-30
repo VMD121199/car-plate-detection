3c26ce0b3b7806ffa0d0ab3968fffaf455b8455a
Minh Duc Vu <56074169+VMD121199@users.noreply.github.com>
2024-01-30 17:24:25 +0100

    update integration (#6)

--- api/main.py
+++ api/main.py
@@ -1,11 +1,16 @@
+from collections import defaultdict
 from fastapi import FastAPI, UploadFile, HTTPException
 import cv2
 import numpy as np
 from keras.models import load_model
 import tempfile
+from ultralytics import YOLO
+
+from util import read_license_plate
 
 app = FastAPI()
 model = load_model("../model/ssd_base.h5")
+yolo_model = YOLO("../model/best.pt")
 
 
 def detect_objects_on_frame(frame):
@@ -22,11 +27,44 @@ def preprocess_frame(image, target_size=(300, 300)):
     return img_array
 
 
+def yolo_detection(frame):
+    yolo_predicted = yolo_model(frame)[0]
+    results = defaultdict(list)
+    for license_plate in yolo_predicted.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = license_plate
+
+        # Crop license plate
+        license_plate_crop = frame[int(y1) : int(y2), int(x1) : int(x2), :]
+
+        # Process license plate
+        license_plate_crop_gray = cv2.cvtColor(
+            license_plate_crop, cv2.COLOR_BGR2GRAY
+        )
+        _, license_plate_crop_thresh = cv2.threshold(
+            license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV
+        )
+
+        # Read license plate number
+        license_plate_text, license_plate_text_score = read_license_plate(
+            license_plate_crop_thresh
+        )
+
+        if license_plate_text is not None:
+            results[class_id] = {
+                "license_plate": {
+                    "bounding_box": [x1, y1, x2, y2],
+                    "text": license_plate_text,
+                    "bbox_score": score,
+                    "text_score": license_plate_text_score,
+                }
+            }
+    return results
+
+
 @app.post("/prediction")
 async def prediction(file: UploadFile):
     if file.content_type.startswith("video"):
         video_bytes = await file.read()
-        # Save the video file to a temporary file
         temp_file_path = tempfile.NamedTemporaryFile(
             delete=False, suffix=".mp4"
         ).name
@@ -42,9 +80,10 @@ async def prediction(file: UploadFile):
             if not ret:
                 break
 
-            processed_frame = preprocess_frame(frame)
-            frame_results = detect_objects_on_frame(processed_frame)
-            results.append(frame_results.tolist())
+            # processed_frame = preprocess_frame(frame)
+            # frame_results = detect_objects_on_frame(processed_frame)
+            image_results_yolo = yolo_detection(frame)
+            results.append(image_results_yolo)
 
         cap.release()
 
@@ -55,11 +94,10 @@ async def prediction(file: UploadFile):
 
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-        processed_image = preprocess_frame(image)
-        image_results = detect_objects_on_frame(processed_image)
-        print(image_results)
-        return {"bounding_box": image_results.tolist()}
-
+        # processed_image = preprocess_frame(image)
+        # image_results = detect_objects_on_frame(processed_image)
+        image_results_yolo = yolo_detection(image)
+        return image_results_yolo[0]
     else:
         raise HTTPException(
             status_code=400,
