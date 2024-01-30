07e0c9f4a1b80faf4419fccb3e92ebeb81a97f7f
Minh-Duc.VU <minhducvu1211@gmail.com>
2024-01-30 07:23:24 +0100

    complete with image

--- api/main.py
+++ api/main.py
@@ -1,11 +1,10 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException
 import cv2
-import io
 import numpy as np
 from keras.models import load_model
 
 app = FastAPI()
-model = load_model("../model/tl_model.h5")
+model = load_model("../model/ssd_base.h5")
 
 
 def detect_objects_on_frame(frame):
@@ -15,54 +14,46 @@ def detect_objects_on_frame(frame):
     return prediction
 
 
-def preprocess_frame(frame, target_size=(300, 300)):
-    return cv2.resize(frame, target_size)
-
-
-def preprocess_image(image, target_size=(300, 300)):
+def preprocess_frame(image, target_size=(300, 300)):
     img = cv2.resize(image, target_size)
-    img_array = np.array(img)
-    img_array = img_array.reshape(1, 300, 300, 3)
+    img_array = np.array(img) / 255
+    img_array = img_array.reshape(1, *target_size, 3)
     return img_array
 
 
-@app.post("/prediction/")
+@app.post("/prediction")
 async def prediction(file: UploadFile):
+    print(file)
     if file.content_type.startswith("video"):
         video_bytes = await file.read()
         video_array = np.frombuffer(video_bytes, dtype=np.uint8)
 
         cap = cv2.VideoCapture(video_array)
-        cap.open(video_array)
 
         results = []
 
         while True:
-            # Read the next frame
-            ret, frame = cap.retrieve()
+            ret, frame = cap.read()
             if not ret:
                 break
 
             processed_frame = preprocess_frame(frame)
-
             frame_results = detect_objects_on_frame(processed_frame)
             results.append(frame_results)
 
         cap.release()
 
         return {"results": results}
-
     elif file.content_type.startswith("image"):
         image_bytes = await file.read()
         image_array = np.frombuffer(image_bytes, dtype=np.uint8)
 
-        # Read the image using OpenCV
         image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
 
-        processed_image = preprocess_image(image)
-
+        processed_image = preprocess_frame(image)
         image_results = detect_objects_on_frame(processed_image)
-        return image_results
+        print(image_results)
+        return {"bounding_box": image_results.tolist()}
 
     else:
         raise HTTPException(
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import requests
+import cv2
+import numpy as np
 
 
 def predict_video(video_file):
-    # Modify the FastAPI URL accordingly
-    fastapi_url = "http://localhost:8000/prediction"
+    fastapi_url = "http://localhost:8000/prediction/"
 
-    files = {"video": video_file.getvalue()}
+    # Send the file content as bytes
+    files = {"file": ("video.mp4", video_file.getvalue(), "video/mp4")}
     response = requests.post(fastapi_url, files=files)
 
     if response.status_code == 200:
@@ -15,25 +17,97 @@ def predict_video(video_file):
         return None
 
 
+def predict_image(image_file):
+    fastapi_url = "http://localhost:8000/prediction/"
+
+    # Send the file content as bytes
+    files = {"file": ("image.jpg", image_file.getvalue(), "image/jpeg")}
+    response = requests.post(fastapi_url, files=files)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+
+def draw_rectangles_on_image(image, bounding_box):
+    x_min, y_min, x_max, y_max = bounding_box
+    return cv2.rectangle(
+        image.copy(), (x_min, y_min), (x_max, y_max), (0, 255, 0), 2
+    )
+
+
+def crop_image(image, bounding_box, target_size=(300, 300)):
+    x_min, y_min, x_max, y_max = bounding_box
+    resized_image = cv2.resize(image, target_size)
+    cropped_image = resized_image[y_min:y_max, x_min:x_max]
+    return resized_image
+
+
 def dashboard():
-    st.title("Video Prediction Dashboard")
+    st.title("Prediction Dashboard")
 
-    video_file = st.file_uploader("Upload Video", type=["mp4", "avi"])
+    prediction_type = st.selectbox(
+        "Select Prediction Type", ["Video", "Image"]
+    )
 
-    if video_file is not None:
-        st.video(video_file)
-        if st.button("Get Prediction"):
-            results = predict_video(video_file)
-            if results:
-                st.subheader("Prediction Results:")
-                if "video_result" in results:
-                    st.subheader("Video Result:")
-                    st.video(results["video_result"])
-                if "text_result" in results:
-                    st.subheader("Text Result:")
-                    st.write(results["text_result"])
-            else:
-                st.error("Error getting prediction. Please try again.")
+    if prediction_type == "Video":
+        video_file = st.file_uploader("Upload Video", type=["mp4", "avi"])
+        if video_file is not None:
+            st.video(video_file)
+            if st.button("Get Prediction"):
+                results = predict_video(video_file)
+                if results:
+                    st.subheader("Prediction Results:")
+                    if "video_result" in results:
+                        st.subheader("Video Result:")
+                        st.video(results["video_result"])
+                    if "text_result" in results:
+                        st.subheader("Text Result:")
+                        st.write(results["text_result"])
+                else:
+                    st.error("Error getting prediction. Please try again.")
+    elif prediction_type == "Image":
+        image_file = st.file_uploader(
+            "Upload Image", type=["jpg", "png", "jpeg"]
+        )
+        if image_file is not None:
+            st.image(
+                image_file, caption="Uploaded Image", use_column_width=True
+            )
+
+            if st.button("Get Prediction"):
+                results = predict_image(image_file)
+                if results:
+                    st.subheader("Prediction Results:")
+                    if "bounding_box" in results:
+                        st.subheader("Plate Detection:")
+
+                        image_array = np.frombuffer(
+                            image_file.getvalue(), dtype=np.uint8
+                        )
+                        original_image = cv2.imdecode(
+                            image_array, cv2.IMREAD_COLOR
+                        )
+                        original_image_rgb = cv2.cvtColor(
+                            original_image, cv2.COLOR_BGR2RGB
+                        )
+
+                        cropped_image = crop_image(
+                            original_image_rgb, results["bounding_box"]
+                        )
+                        image_with_rectangles = draw_rectangles_on_image(
+                            cropped_image, results["bounding_box"]
+                        )
+                        st.image(
+                            image_with_rectangles,
+                            caption="Cropped Image with Rectangles",
+                            use_column_width=True,
+                            width=300,
+                        )
+
+                else:
+                    st.error("Error getting prediction. Please try again.")
 
 
 if __name__ == "__main__":
