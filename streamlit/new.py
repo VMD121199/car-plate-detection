b9d2c17e20d107fc8e7e60c2a5bcbc84cc19d8d5
Minh Duc Vu <56074169+VMD121199@users.noreply.github.com>
2024-02-01 00:25:24 +0100

    Delete streamlit/new.py

--- streamlit/new.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import tempfile
-import os
-
-# Upload video file or use a default video file
-video_file = st.file_uploader("Upload Video", type=["mp4", "avi"])
-if video_file is not None:
-    st.video(video_file)
